70275c56c49d608c45ee961f546b3e078485df01
Chloe McCarty <chloe_mccarty@hotmail.com>
2018-04-18 15:28:23 -0400

    clean up

--- src/raft/NodeRunner.java
+++ src/raft/NodeRunner.java
@@ -1,9 +1,6 @@
 package raft;
 
-import raft.Node.*;
-
-// extends thread, because we will run one of these for each Node in the cluster
-public class NodeRunner extends Thread {
+public class NodeRunner {
     public static void main(String[] args) {
         // initialize the node (all nodes are followers when first initialized)
         // the follower constructor also initializes based on some configuration file
