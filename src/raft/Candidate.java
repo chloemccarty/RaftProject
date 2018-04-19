a31fe720e495e2cc024c76c7422a8a5c772d9a84
Elijah <enavarro9696@yahoo.com>
2018-04-19 17:48:18 -0400

    changes

--- src/raft/Candidate.java
+++ src/raft/Candidate.java
@@ -9,6 +9,21 @@ public class Candidate extends Node {
     @Override
     public Node run() {
         // returns either a leader or a follower, but never another candidate
+        term++;
+
+        // vote for self
+
+        // reset election timer
+
+        // requestVote() RPC
+
+        // if (votesReceived >= (n/2)+1
+        //  state = leader
+        // else if appendEntries received
+        //  state = follower
+        // else if electionTimeout
+        //  ???
+        // return state
         return null;
     }
 }
--- src/raft/Node.java
+++ src/raft/Node.java
@@ -1,12 +1,20 @@
 package raft;
+import raft.RequestVote.Candidate;
 
 public abstract class Node {
     int id;
     int term;
     NodeType state;
+    // TODO should all nodes have a queue to hold results of votes?
     enum NodeType {FOLLOWER, CANDIDATE, LEADER};
     // log
+    // arraylist of int terms and String cmd;
+    // class LogEntry {
+    // int term;
+    // String cmd;
+    // }
 
+    Candidate.Builder c = Candidate.newBuilder();
     public abstract void respondToRequestVote();
 
     /**
