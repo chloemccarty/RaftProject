90451bdc69206bc1651ac6b1a8d8e3eff3b236ee
Chloe McCarty <chloe_mccarty@hotmail.com>
2018-04-19 15:23:07 -0400

    group collab

--- src/raft/Candidate.java
+++ src/raft/Candidate.java
@@ -1,12 +1,19 @@
 package raft;
 
-import java.util.ArrayList;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Candidate extends Node {
+    long electionTimer;
 
     public Candidate(Node node) {
         super(node);
     }
+
+
+
 
     @Override
     public void respondToRequestVote() {
@@ -14,9 +21,51 @@ public class Candidate extends Node {
     }
 
     @Override
-    public Node run() {
+    public Node run() throws IOException {
         apply();
-        // returns either a leader or a follower, but never another candidate
+
+        boolean leader = startElection();
+        if (leader) {
+            // return a leader Node
+            return new Leader(this);
+        }
+        else {
+            // if we received a heartbeat, there's a new leader
+            // return follower
+            // otherwise, no leader was elected, return a new candidate
+        }
+
+        // returns either a leader or a follower
         return null;
+    }
+
+    // send out RequestVote RPCs to all other nodes
+    private boolean startElection() throws IOException {
+        // build message
+        term++;
+        int votes = 1;
+
+        RequestVote.RequestVoteMessage.Builder builder = RequestVote.RequestVoteMessage.newBuilder();
+        builder.setCandidateId(this.id);
+        builder.setTerm(this.term);
+        RequestVote.RequestVoteMessage rvm = builder.build();
+
+        // TODO implement with AppendEntries Stuff
+        // builder.setLastLogIndex();
+        // builder.setLastLogTerm();
+
+        // send to each socket
+        for (Socket socket : config) {
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            rvm.writeDelimitedTo(out);
+        }
+
+        // TODO add an Message object which extends thread. Instantiate one for each node in the configuration
+        // it will send out the node and also listen for a response and return.
+
+
+        return votes >= (numNodes + 1) / 2;
     }
 }
--- src/raft/Node.java
+++ src/raft/Node.java
@@ -3,6 +3,8 @@ package raft;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -12,12 +14,13 @@ import java.nio.file.Path;
 public abstract class Node {
     int id;
     int term;
-
+    int numNodes;
     int votedFor;
     int commitIndex;
     int lastApplied;
     List<LogEntry> log;
-    List<InetAddress> config;
+    List<Socket> config;
+    final int PORT = 6666;
 
 
     public Node(Node that) {
@@ -41,7 +44,7 @@ public abstract class Node {
      *
      * @return An instance of a node that this node becomes after running (e.g. if a change from candidate to leader is necessary)
      */
-    public abstract Node run();
+    public abstract Node run() throws IOException;
 
     /**
      * All nodes must apply new logs if commitIndex > lastApplied
@@ -54,6 +57,10 @@ public abstract class Node {
      * Read IP addresses of nodes from config file.
      */
     public void initConfig() throws IOException {
+
+        // TODO have a loop that continually accepts requests to this socket
+        ServerSocket server = new ServerSocket(PORT);
+
         // be sure not to add self to list of nodes to send to
         InetAddress thisIP = InetAddress.getLocalHost();
 
@@ -61,10 +68,12 @@ public abstract class Node {
         for (String ip : ips) {
             InetAddress i = InetAddress.getByName(ip);
             if (thisIP != i) {
-                config.add((i));
+                config.add(new Socket(i, PORT));
             }
-        }
 
+        }
+        numNodes = ips.size();
     }
+
 
 }
