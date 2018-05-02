89ee49bf15757377a84eadf1d6a3e7f24981d1fb
jeisenhower <jme006@lvc.edu>
2018-05-02 09:17:47 -0400

    Updated nextIndex and matchIndex

--- src/raft/Leader.java
+++ src/raft/Leader.java
@@ -3,11 +3,36 @@ package raft;
 import client.Client;
 import connect.Network;
 
+import java.util.HashMap;
+
 public class Leader extends Node {
+    boolean forfeit = false;
+    int nextIndex[];
+
+    int matchIndex[];
 
     public Leader(Node node) {
         super(node);
-        NodeRunner.client.log("Initializing as leader...");
+
+        //Initiate matchIndex and nextIndex. Create arrays corresponding to ip address. So with each ip address sent, we have
+        //a corresponding place in the array. Could we use a hashmap?
+
+        //Could we make this an arrayList?
+        matchIndex = new int[numNodes];
+        for (int i=0; i<numNodes; i++) {
+            //Initialized to zero for each index, which corresponds to each node
+            matchIndex[i] = 0;
+        }
+
+        //Store a nextIndex value for each node in the cluster. If nextIndex does not match the matchIndex value for a node, we must decrement
+        //matchIndex and send another message to that follower. If follower resturns false, decrement again. Continue this until terms match,
+        //then delete all previous log entries.
+        nextIndex = new int[numNodes];
+        for (int i=0; i<numNodes; i++) {
+            //Initialized to leader's last log index +1, for each index, which corresponds to each node
+            nextIndex[i] = this.log.size();
+        }
+        System.out.println("Initializing as leader...");
     }
 
     @Override
@@ -17,9 +42,25 @@ public class Leader extends Node {
             return;
         if (message.type == Message.MessageType.APPEND_ENTRIES) {
             // TODO
+            AppendEntries.AppendEntriesMessage ae = (AppendEntries.AppendEntriesMessage) message.message;
+            if (ae.getTerm() > this.term) forfeit = true;
         }
         else if (message.type == Message.MessageType.APPEND_ENTRIES_RESPONSE) {
             // TODO
+            AppendEntries.Response aer = (AppendEntries.Response) message.message;
+            if (aer.getSuccess()) {
+                //increment nextIndex and set matchIndex equal to nextIndex for the corresponding follower from which the message was received.
+                int index = nextIndex[aer.getFollowerId()];
+                nextIndex[aer.getFollowerId()] = index+1;
+                //update matchIndex to one less than the updated nextIndex value.
+                matchIndex[aer.getFollowerId()] = index;
+            }
+
+            else{
+                //Decrement nextIndex and keep matchIndex the same.
+                int index = nextIndex[aer.getFollowerId()];
+                nextIndex[aer.getFollowerId()] = index-1;
+            }
         }
         else if (message.type == Message.MessageType.REQUEST_VOTES) {
             respondToRequestVote(message);
@@ -32,17 +73,16 @@ public class Leader extends Node {
     @Override
     public Node run() {
         while (true) {
+            //Need to handle updating commit index within the apply method
             apply();
             // check messages
             // react to messages
             // send response
             Message m = checkForInput();
             if (m != null) {
-                NodeRunner.client.log("Message received by leader");
+                System.out.println("Message received by leader");
                 handleMessage(m);
-                // TODO find out why this isn't running
                 if (forfeit) {
-                    NodeRunner.client.log("Reverting to follower state");
                     return new Follower(this);
                 }
             }
