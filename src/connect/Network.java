3d7afd6875e4fd0ef83e875440c294a78a2bb219
Chloe McCarty <chloe_mccarty@hotmail.com>
2018-05-07 10:53:45 -0400

    fix !

--- src/connect/Network.java
+++ src/connect/Network.java
@@ -13,7 +13,7 @@ public class Network {
     // public static void send(int type, byte[] data, String IP) {
     public static void send(Message.MessageType type, GeneratedMessageV3 data, String IP) {
         NodeRunner.client.log("Sending message");
-        if (IP.equalsIgnoreCase(NodeRunner.node.config.get(NodeRunner.node.id)))
+        if (!IP.equalsIgnoreCase(NodeRunner.node.config.get(NodeRunner.node.id)))
             new Sender((byte) type.value, data, IP).start();
     }
 
