57d83c0e397b0e8cff65ccd5379881ab832bf3b9
Chloe McCarty <chloe_mccarty@hotmail.com>
2018-05-07 15:01:17 -0400

    bug

--- src/raft/Follower.java
+++ src/raft/Follower.java
@@ -51,7 +51,7 @@ public class Follower extends Node {
             // Ignore the message and return false, decrement nextIndex on the leader server
             confirm = false;
         }
-        else if (ae.getPrevLogIndex() == -1 || ae.getPrevLogTerm() == -1 || ae.getEntriesList() == null || ) {
+        else if (ae.getPrevLogIndex() == -1 || ae.getPrevLogTerm() == -1 || ae.getEntriesList() == null) {
             // Log is empty and the message must be a heartbeat.
             confirm = true;
             heartbeat = true;
